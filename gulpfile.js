b8e61ba0403b63d15c84b98488eb848aa17ee1a7
camel.chang <camel.chang@hopebaytech.com>
2017-02-02 18:10:29 +0800

    add scss

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var scssSource = './scss/*.scss';
+
+gulp.task('sass', function() {
+    gulp.src(scssSource)
+        .pipe(sass({outputStyle: 'compressed', sourceComments: true}))
+        .pipe(gulp.dest(function(f) {
+            return "./dist/css";
+        }));
+});
+
+gulp.task('compileSCSS', ['sass'], function() {
+    gulp.watch(scssSource, ['sass']);
+})
